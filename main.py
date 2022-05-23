2940d7b11dc7f3a729bff38223937b9cf1fc6b3d
Dakota Marshall <me@dakotamarshall.net>
2022-05-23 21:39:26 +0000

    Add selenium testing I was doing today

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import selenium
+from selenium import webdriver
+
+
+def get_comic_link(date):
+    driver = webdriver.Firefox()
+    driver.get("https://www.gocomics.com/heathcliff/2021/04/19")
+    
+    comic_obj = driver.find_element_by_xpath('//img[contains(@alt, "Heathcliff Comic Strip for")]')
+    comic_link = comic_obj.get_attribute("src")
+
+    return comic_link
