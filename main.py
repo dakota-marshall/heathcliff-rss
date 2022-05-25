5df9da9cc8de3f3982bcd7315f00e2a6a9b8894c
Dakota Marshall <me@dakotamarshall.net>
2022-05-25 20:52:39 +0000

    Finish get_comic_link

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import selenium, sqlite3, datetime
+import sqlite3, datetime
 from selenium import webdriver
-
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 def get_comic_link(day, month, year):
-    # Start Firefox WebDriver instance
-    driver = webdriver.Firefox()
+    # Start Firefox WebDriver instance and sert to headless
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.headless = True
+    driver = webdriver.Firefox(options=firefox_options)
 
     # Set and grab todays link
     link = f"""https://www.gocomics.com/heathcliff/{year}/{month}/{day}"""
@@ -16,9 +18,11 @@ def get_comic_link(day, month, year):
     comic_obj = driver.find_element_by_xpath('//img[contains(@alt, "Heathcliff Comic Strip for")]')
     comic_link = comic_obj.get_attribute("src")
 
+    driver.quit()
+
     return comic_link
 
-def main()
+def main():
 
     # Get today's date
     date = datetime.datetime.now()
@@ -27,7 +31,10 @@ def main()
     year = date.strftime("%Y")
 
     # Get today's comic link
-    get_comic_link(day, month, year)
+    comic_link = get_comic_link(day, month, year)
+
+    print(comic_link)
+
 
 if __name__ == "__main__":
   main()
