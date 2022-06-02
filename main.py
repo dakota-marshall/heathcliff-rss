eb323c978815e7eecc9283d753346ea02115f2c3
Dakota Marhsall <me@dakotamarshall.net>
2022-06-02 00:15:54 -0400

    Add SQLite database and saving function

--- main.py
+++ main.py
@@ -22,6 +22,28 @@ def get_comic_link(day, month, year):
 
     return comic_link
 
+def database_save(date, url):
+
+    # Create SQLite connection
+    # This assumes the 'posts' table has been made with this schema ("date" TEXT, "img_url" TEXT)
+    conn = sqlite3.connect('heathcliff.db')
+    cursor = conn.cursor()
+
+    # Query if date is already in DB
+    cursor.execute(f"""SELECT * FROM posts WHERE date = '{date}'""")
+    result = cursor.fetchall()
+    
+    # Return if we get anything back from the query
+    if any(result):
+        conn.close()
+        return 1
+    
+    # Write new entry if not
+    cursor.execute(f"""INSERT INTO posts (date, img_url) VALUES ('{date}', '{url}')""")
+    conn.commit()
+    conn.close()
+    return 0
+
 def main():
 
     # Get today's date
@@ -29,12 +51,15 @@ def main():
     day = date.strftime("%d")
     month = date.strftime("%m")
     year = date.strftime("%Y")
+    date_string = date.strftime("%Y-%m-%d")
 
     # Get today's comic link
     comic_link = get_comic_link(day, month, year)
+    
+    #Write URL to database
+    database_save(date_string, comic_link)
 
-    print(comic_link)
-
+    # Generate New RSS Post
 
 if __name__ == "__main__":
   main()
